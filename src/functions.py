40bc0fa5f55557c029fd19ef7e8e7e89a452e109
Magomed <scrythepvp@gmail.com>
2022-04-13 11:52:08 +0200

    refactor: create Game Class for game
    rewritten game functions inside a game class function

--- src/functions.py
+++ src/functions.py
@@ -1,22 +1,6 @@
 import pygame
 
 
-def move(keys, object):
-    if keys[pygame.K_w]:
-        object.move_upwards()
-    if keys[pygame.K_d]:
-        object.move_right()
-    if keys[pygame.K_s]:
-        object.move_downwards()
-    if keys[pygame.K_a]:
-        object.move_left()
-
-
 def scale_image(img, factor):
     size = round(img.get_width() * factor), round(img.get_height() * factor)
     return pygame.transform.scale(img, size)
-
-
-def collision(player, obstacle_group):
-    return pygame.sprite.spritecollide(
-        player, obstacle_group, False, pygame.sprite.collide_mask)
--- src/main.py
+++ src/main.py
@@ -1,41 +1,77 @@
 import pygame
-from sys import exit
 from Player import Heart
 from Bone import Bone
 from Battle_Box import Battle_Box
-from functions import move, collision
 
-pygame.init()
-TOTAL_WIDTH = 960
-TOTAL_HEIGHT = 720
-SCREEN = pygame.display.set_mode((TOTAL_WIDTH, TOTAL_HEIGHT))
-SCREEN_RECT = SCREEN.get_rect()
-battle_Box = Battle_Box(SCREEN_RECT)
-FPS = 60
-clock = pygame.time.Clock()
 
-heart = Heart(battle_Box.get_box(), battle_Box.get_border())
-player = pygame.sprite.GroupSingle()
-player.add(heart)
-bone = Bone(battle_Box.get_box())
-bones_group = pygame.sprite.Group()
-bones_group.add(bone)
+class Game:
+    TOTAL_WIDTH = 960
+    TOTAL_HEIGHT = 720
+    FPS = 60
 
-while True:
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+    def __init__(self):
+        self.screen = pygame.display.set_mode(
+            (self.TOTAL_WIDTH, self.TOTAL_HEIGHT))
+        self.screen_rect = self.screen.get_rect()
+        self.battle_Box = Battle_Box(self.screen_rect)
+        self.heart = Heart(self.battle_Box.get_box(),
+                           self.battle_Box.get_border())
+        self.player = pygame.sprite.GroupSingle()
+        self.player.add(self.heart)
+        self.bone = Bone(self.battle_Box.get_box())
+        self.bones_group = pygame.sprite.Group()
+        self.bones_group.add(self.bone)
 
-    keys = pygame.key.get_pressed()
-    move(keys, heart)
+        self.done = False
+        self.clock = pygame.time.Clock()
 
-    if not heart.dead and collision(player.sprite, bones_group):
-        heart.take_damage()
+    def run(self):
+        # update all sprite groups
+        # draw all sprite #groups
+        # others such as event stuff
+        while not self.done:
+            self.event_loop()
+            self.update()
+            self.draw()
+            pygame.display.flip()
+            self.clock.tick(self.FPS)
 
-    battle_Box.draw(SCREEN)
-    player.draw(SCREEN)
-    bones_group.draw(SCREEN)
-    heart.draw_hp(SCREEN)
-    pygame.display.update()
+    def event_loop(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.done = True
+
+    def update(self):
+        # check if heart collides with any groups
+        # and also other updates like movement
+        keys = pygame.key.get_pressed()
+        self.move(keys, self.heart)
+        if not self.heart.dead and self.collision():
+            self.heart.take_damage()
+
+    def draw(self):
+        self.battle_Box.draw(self.screen)
+        self.player.draw(self.screen)
+        self.bones_group.draw(self.screen)
+        self.heart.draw_hp(self.screen)
+
+    def move(self, keys, object):
+        if keys[pygame.K_w]:
+            object.move_upwards()
+        if keys[pygame.K_d]:
+            object.move_right()
+        if keys[pygame.K_s]:
+            object.move_downwards()
+        if keys[pygame.K_a]:
+            object.move_left()
+
+    def collision(self):
+        return pygame.sprite.spritecollide(
+            self.player.sprite, self.bones_group, False, pygame.sprite.collide_mask)
+
+
+if __name__ == '__main__':
+    pygame.init()
+    game = Game()
+    game.run()
+    pygame.quit()
