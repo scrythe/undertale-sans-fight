2aeb914d394bea93336195d76eb3a18d4f68f288
Magomed <scrythepvp@gmail.com>
2022-04-15 10:14:52 +0200

    refactor: refactor and clean bone_wall + bone more

--- src/bone.py
+++ src/bone.py
@@ -1,3 +1,4 @@
+from venv import create
 import pygame
 from utils import get_scaled_image
 from pygame import Surface, Rect
@@ -10,24 +11,45 @@ class Bone(pygame.sprite.Sprite):
         self.image = get_scaled_image('sprites/spr_s_bonestab_v.png')
         self.rect = self.image.get_rect(topleft=coords)
         self.mask = pygame.mask.from_surface(self.image)
+        # Create a repositioned rect for drawing onto surface
+        self.rect_in_box = self.rect.copy()
+        # box: Rect = pygame.Rect((0, 0), (0, 0))
+        # self.create_rect_inside_surf(box)
 
     def get_default_rect(self):
         return self.rect
 
-    def draw_onto_surf(self, screen: Surface, box: Rect):
-        offset = self.rect.left - box.left, self.rect.top - box.top
-        screen.blit(self.image, offset)
+    def create_rect_inside_surf(self, box: Rect):
+        # Repositioned Rectangle inside a Surface
+        self.rect_in_box.x = self.rect.left - box.left
+        self.rect_in_box.y = self.rect.top - box.top
 
-    def get_coords_inside_surf(self, box: Rect):
-        offset = self.rect.left - box.left, self.rect.top - box.top
-        return offset
+    def draw_onto_surf(self, screen: Surface):
+        screen.blit(self.image, self.rect_in_box)
+
+    def go_upwards(self, amount):
+        self.rect.y -= amount
+        self.rect_in_box.y -= amount
+
+    def go_right(self, amount):
+        self.rect.x += amount
+        self.rect_in_box.y += amount
+
+    def go_downwards(self, amount):
+        self.rect.y += amount
+        self.rect_in_box.y += amount
+
+    def go_left(self, amount):
+        self.rect.x -= amount
+        self.rect_in_box.x -= amount
 
 
 class Bone_Group:
     def __init__(self, battle_box: Rect):
         self.bone_group = pygame.sprite.Group()
-        # surface with all the bones
+        # Surface with all the bones
         self.battle_box = battle_box
+        # surface as big as battle box where bones will be drawn onto
         self.surface = pygame.Surface(
             (self.battle_box.width, self.battle_box.height))
         self.surface.set_colorkey((0, 0, 0))
@@ -40,6 +62,7 @@ class Bone_Group:
         default_rect.midtop = (self.battle_box.left, self.battle_box.bottom)
         for index in range(amount_bones):
             bone = Bone((default_rect.x, default_rect.y))
+            bone.create_rect_inside_surf(self.surface_rect)
             self.bone_group.add(bone)
             default_rect.x += steps
         self.bone_group_sprites: List[Bone] = self.bone_group.sprites()
@@ -50,5 +73,10 @@ class Bone_Group:
     def draw_bones(self):
         self.surface.fill('black')
         for bone in self.bone_group_sprites:
-            bone.draw_onto_surf(self.surface, self.surface_rect)
+            bone.draw_onto_surf(self.surface)
         self.surface.set_colorkey((0, 0, 0))
+
+    def get_distance_from_bottom(self):
+        first_bone = self.bone_group_sprites[0]
+        distance_from_bottom = self.surface_rect.height - first_bone.rect_in_box.top
+        return distance_from_bottom
--- src/bone_wall.py
+++ src/bone_wall.py
@@ -7,7 +7,7 @@ class Bone_Wall(Bone_Group):
         super().__init__(battle_box)
         self.amount_bones = 14
         self.bone_group_width = self.battle_box.width
-        self.max_height = self.battle_box.height / 1.5
+        self.max_height = self.battle_box.height / 3
         self.attack_speed = 8
         super().create_bone_wall(self.amount_bones, self.bone_group_width)
         self.attack_state = False
@@ -16,9 +16,8 @@ class Bone_Wall(Bone_Group):
         self.attack_state = True
 
     def check_end_of_attack(self):
-        first_bone = self.bone_group_sprites[0]
-
-        if first_bone.get_coords_inside_surf(self.surface_rect)[1] <= self.max_height:
+        height_of_attack = self.get_distance_from_bottom()
+        if height_of_attack >= self.max_height:
             return True
 
     def attack(self):
@@ -31,7 +30,7 @@ class Bone_Wall(Bone_Group):
 
     def go_upwards(self):
         for bone in self.bone_group_sprites:
-            bone.rect.y -= self.attack_speed
+            bone.go_upwards(self.attack_speed)
 
     def update(self):
         self.attack()
